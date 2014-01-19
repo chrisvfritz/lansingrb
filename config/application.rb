a996224b40c98d6390f85c152cd0000877c860ba
Chris Fritz <chrisvfritz@gmail.com>
2014-01-19 00:05:26 -0500

    trying to solve problem with forem and heroku

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module Lansingrb
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
+
+    config.assets.initialize_on_precompile = false
+    routes.named_routes[:forem] ||= Journey::Route.new(nil, nil, Journey::Path::Pattern.new("\forums"), {})
   end
 end
